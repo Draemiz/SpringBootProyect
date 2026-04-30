b39a8b9dade26461c8942d1bfb9e411813ab5657
Draemiz <draemizgx1@gmail.com>
2026-04-29 23:10:23 -0500

    Segund commit proyecto Spring Boot

---
+++ src/main/java/com/krakedev/asistencias/dto/Asistencia.java
@@ -0,0 +1,5 @@
+package com.krakedev.asistencias.dto;
+
+public class Asistencia {
+
+}
---
+++ src/main/java/com/krakedev/asistencias/dto/Estudiante.java
@@ -0,0 +1,12 @@
+package com.krakedev.asistencias.dto;
+
+public class Estudiante {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	
+	public Estudiante() {
+
+	}
+}
