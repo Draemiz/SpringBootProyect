f0faff568e4f9f431c81bac089043a61a03c938a
Draemiz <draemizgx1@gmail.com>
2026-04-29 23:15:57 -0500

    creada la clase asistencia

--- src/main/java/com/krakedev/asistencias/dto/Estudiante.java
+++ src/main/java/com/krakedev/asistencias/dto/Estudiante.java
@@ -1,5 +1,7 @@
 package com.krakedev.asistencias.dto;
 
+
+
 public class Estudiante {
 	
 	private String cedula;
@@ -7,6 +9,39 @@ public class Estudiante {
 	private String apellido;
 	
 	public Estudiante() {
+		
+		
 
 	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+	
+	
 }
