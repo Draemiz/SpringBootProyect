d6fce6dd109509339fb3a69b41c0f0193f47636f
Draemiz <draemizgx1@gmail.com>
2026-04-29 23:30:22 -0500

    creada la clase estudiante getters y setters con localdate y localtime

--- src/main/java/com/krakedev/asistencias/dto/Asistencia.java
+++ src/main/java/com/krakedev/asistencias/dto/Asistencia.java
@@ -1,5 +1,37 @@
 package com.krakedev.asistencias.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Asistencia {
+	
+	private LocalDate fechaClase;
+	private LocalDateTime fechaHoraRegistro;
+	private String estado; //P-Presente A-Ausente
+	public LocalDate getFechaClase() {
+		return fechaClase;
+	}
+	public void setFechaClase(LocalDate fechaClase) {
+		this.fechaClase = fechaClase;
+	}
+	public LocalDateTime getFechaHoraRegistro() {
+		return fechaHoraRegistro;
+	}
+	public void setFechaHoraRegistro(LocalDateTime fechaHoraRegistro) {
+		this.fechaHoraRegistro = fechaHoraRegistro;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	@Override
+	public String toString() {
+		return "Asistencia [fechaClase=" + fechaClase + ", fechaHoraRegistro=" + fechaHoraRegistro + ", estado="
+				+ estado + "]";
+	}
+	
+	
 
 }
