2b7a89e5ebf62a3574b3ff0938810c24f408850c
Draemiz <draemizgx1@gmail.com>
2026-05-03 13:22:09 -0500

    mejora en clase aistencia

--- src/main/java/com/krakedev/asistencias/dto/Asistencia.java
+++ src/main/java/com/krakedev/asistencias/dto/Asistencia.java
@@ -4,34 +4,40 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Asistencia {
-	
-	private LocalDate fechaClase;
-	private LocalDateTime fechaHoraRegistro;
-	private String estado; //P-Presente A-Ausente
-	public LocalDate getFechaClase() {
-		return fechaClase;
-	}
-	public void setFechaClase(LocalDate fechaClase) {
-		this.fechaClase = fechaClase;
-	}
-	public LocalDateTime getFechaHoraRegistro() {
-		return fechaHoraRegistro;
-	}
-	public void setFechaHoraRegistro(LocalDateTime fechaHoraRegistro) {
-		this.fechaHoraRegistro = fechaHoraRegistro;
-	}
-	public String getEstado() {
-		return estado;
-	}
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
+
+    private LocalDate fechaClase;
+    private LocalDateTime fechaHoraRegistro;
+    private String estado;
+
+    public LocalDate getFechaClase() {
+        return fechaClase;
+    }
+
+    public void setFechaClase(LocalDate fechaClase) {
+        this.fechaClase = fechaClase;
+    }
+
+    public LocalDateTime getFechaHoraRegistro() {
+        return fechaHoraRegistro;
+    }
+
+    public void setFechaHoraRegistro(LocalDateTime fechaHoraRegistro) {
+        this.fechaHoraRegistro = fechaHoraRegistro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 	
 	@Override
 	public String toString() {
-		return "Asistencia [fechaClase=" + fechaClase + ", fechaHoraRegistro=" + fechaHoraRegistro + ", estado="
+		return "Asistencia fechaClase=" + fechaClase + ", fechaHoraRegistro=" + fechaHoraRegistro + ", estado="
 				+ estado + ", getFechaClase()=" + getFechaClase() + ", getFechaHoraRegistro()=" + getFechaHoraRegistro()
 				+ ", getEstado()=" + getEstado() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
-				+ ", toString()=" + super.toString() + "]";
+				+ ", toString()=" + super.toString() + ".";
 	}
 }
